8008e9ace3e6d3f96fd4ef661582cf2ac2fc3802
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2023-10-25 12:51:29 -0300

    Add mobile menu toggle

--- js/nav.js
+++ js/nav.js
@@ -3,18 +3,36 @@ const createNav = () => {
 
     nav.innerHTML = `
         <div class="nav">
-            <a href="index.html"><img src="img/dark-logo.png" class="brand-logo" alt=""></a>
+            
+            <div class="menu-toggle" id="menu-toggle">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </div>
+
+            <ul class="categories">
+                <li><a href="#sale" class="link">sales</a></li>
+                <li><a href="#clothes" class="link">clothes</a></li>
+                <li><a href="#shoes" class="link">shoes</a></li>
+                <li><a href="#accessories" class="link">accessories</a></li>
+            </ul>
+            
+            <a href="index.html"><img src="img/carri-logo1.png" class="brand-logo" alt=""></a>
+            
             <div class="nav-items">
+
                 <div class="search">
                     <input type="text" class="search-box" placeholder="search brand, product">
                     <button class="search-btn">search</button>
                 </div>
+
                 <a href="#"><img src="img/user.png" alt=""></a>
                 <a href="cart.html"><img src="img/cart.png" alt="">
                     <span id="cart-number" class="cart-number">0</span>
                 </a>
             </div>
         </div>
+        
         <ul class="links-container">
             <li class="link-item"><a href="index.html" class="link">home</a></li>
             <li class="link-item"><a href="#sale" class="link">sales</a></li>
@@ -24,6 +42,16 @@ const createNav = () => {
             <li class="link-item"><a href="#footer" class="link">about us</a></li>
         </ul>
     `;
+
+    document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const categories = document.querySelector(".categories");
+
+    menuToggle.addEventListener("click", function () {
+        menuToggle.classList.toggle("active");
+        categories.classList.toggle("active");
+    });
+});
 }
 
 createNav();
